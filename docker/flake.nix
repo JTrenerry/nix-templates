c821bf32c7178109942fa8ded2e1daceb7c360f1
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2024-08-22 21:05:29 +1000

    dockerv2

--- docker/flake.nix
+++ docker/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix-flake-based Java development environment";
+  description = "A Nix-flake-based Docker development environment";
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
     flake-utils.url = "github:numtide/flake-utils";
